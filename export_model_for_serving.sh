5361ea487e1b3e4462b2b51f68fcfa0899806c1c
Zi Yin <zi@moveworks.ai>
2019-02-13 11:53:23 -0800

    update

--- export_model_for_serving.sh
+++ export_model_for_serving.sh
@@ -1,6 +1,6 @@
 source="consolidated_qa_qd"
 data_dir=../qa_stackoverflow/seq2seq_data/$source
-grpc_port=10500
+grpc_port=8500
 model_dir_in=models/model_$source
 model_dir_out=serving_models/model_$source
 port=$(($grpc_port+1))
@@ -10,16 +10,16 @@ echo "service_name=$service_name"
 
 docker kill $service_name
 docker rm $service_name
-rm -r $model_dir_out/154*
+rm -r $model_dir_out/15*
 python -m nmt.nmt \
     --out_dir=$model_dir_in \
     --export_path=$model_dir_out \
     --ckpt_path=$model_dir_in \
-    --model=generative \
-    --infer_mode="beam_search" \
-    --beam_width=10
-docker run --name $service_name -p $port:8501 --mount type=bind,source=$pwd/$model_dir_out,target=/models/seq2seq -e MODEL_NAME=seq2seq -t tensorflow/serving
+    --model=discriminative
+#    --infer_mode="beam_search" \
+#    --beam_width=10
+#docker run --name $service_name -p $port:8501 --mount type=bind,source=$pwd/$model_dir_out,target=/models/model_$source -e MODEL_NAME=model_$source -t tensorflow/serving
+docker run --name $service_name -p $port:8501 --mount type=bind,source=$pwd/$model_dir_out,target=/models/model_$source -e MODEL_NAME=model_$source -t zi/tensorflow-serving:latest
 
 # enables GPU serving
-# docker run --name $service_name --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=0 -p $port:8501 --mount type=bind,source=$pwd/$model_dir_out,target=/models/seq2seq -e MODEL_NAME=seq2seq -t tensorflow/serving:latest-gpu
-
+# docker run --name $service_name --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=0 -p $port:8501 --mount type=bind,source=$pwd/$model_dir,target=/models/model_$source -e MODEL_NAME=model_$source -t tensorflow/serving:latest-gpu

19a46b3ecf355ff52479be093a64216b7145265f
Zi Yin <zi@moveworks.ai>
2019-02-15 14:19:47 -0800

    update

--- train_qa.sh
+++ train_qa.sh
@@ -1,4 +1,5 @@
-source="superuser.com"
+#source="consolidated_qa_qd"
+source="consolidated_querydata"
 data_dir=../qa_stackoverflow/seq2seq_data/$source
 model_dir=models/model_qa_$source
 mkdir -p $model_dir
@@ -6,13 +7,13 @@ python -m nmt.nmt \
     --src=doc --tgt=query \
     --vocab_prefix=$data_dir/vocab.text \
     --train_prefix=$data_dir/train.text \
-    --dev_prefix=$data_dir/dev.text \
-    --test_prefix=$data_dir/dev.text \
+    --dev_prefix=$data_dir/test.text \
+    --test_prefix=$data_dir/test.text \
     --out_dir=$model_dir \
-    --num_train_steps=120000 \
+    --num_train_steps=240000 \
     --steps_per_stats=100 \
     --num_layers=4 \
-    --num_units=200 \
+    --num_units=300 \
     --batch_size=64 \
     --dropout=0.2 \
     --metrics=bleu \
